cb2393ae06957b1a110ad7269c966503c67acfc8
mathunevi <mathubharathi1994@gmail.com>
2020-01-20 22:48:02 +0530

    source

---
+++ src/org/emp/Employee1.java
@@ -0,0 +1,37 @@
+package org.emp;
+
+public class Employee1 {
+public void empId() {
+	System.out.println("234");
+	
+}
+public void empName() {
+	System.out.println("mathu");
+	
+}
+																	 public void empDob() {
+																		 System.out.println("16.7.94");
+																		
+																	}
+																	 public void empPhone() {
+																		 System.out.println("9626338362");
+																		 
+																		
+																	}
+																	public void empEmail() {
+																		System.out.println("mathubharathi1994@gmail.com");
+																		
+																	}
+																	public void empAddress() {
+																		System.out.println("thalambur");
+																	System.out.println("chennai");	
+}
+public static void main(String[] args) {
+	Employee1 e=new Employee1();
+	e.empName();
+	e.empDob();
+	e.empPhone();
+	e.empEmail();
+	e.empAddress();
+}
+}
